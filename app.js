e98ad39681333035035340eefc577a17f6ac0887
nicopetelo <nicopetelo@gmail.com>
2021-05-12 11:14:57 -0700

    commenting out 9.1

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    // for (i = 0; i < profileDataArr.length; i++) {
-    //     console.log(profileDataArr[i]);
-    // }
+// Lesson 9.1
+// const printProfileData = (profileDataArr) => {
+//     // for (i = 0; i < profileDataArr.length; i++) {
+//     //     console.log(profileDataArr[i]);
+//     // }
 
-    // profileDataArr.forEach((profileItem) => {
-    //     console.log(profileItem);
-    // });
+//     // profileDataArr.forEach((profileItem) => {
+//     //     console.log(profileItem);
+//     // });
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
 
-    // in terms of functionality, lines 5-7 = 9-11 = 13
-};
+//     // in terms of functionality, lines 5-7 = 9-11 = 13
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
