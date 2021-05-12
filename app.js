0b3b7802f6b2a57b91db65a3ea7ebdf57c5e8cd4
nicopetelo <nicopetelo@gmail.com>
2021-05-12 10:53:55 -0700

    pulling in user input

---
+++ app.js
@@ -0,0 +1,18 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    // for (i = 0; i < profileDataArr.length; i++) {
+    //     console.log(profileDataArr[i]);
+    // }
+
+    // profileDataArr.forEach((profileItem) => {
+    //     console.log(profileItem);
+    // });
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+    // in terms of functionality, lines 5-7 = 9-11 = 13
+};
+
+printProfileData(profileDataArgs);
